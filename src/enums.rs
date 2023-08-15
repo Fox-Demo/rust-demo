1c3c3888bc131660535e0fab6c0d6b501405f2c4
LI-YONG-QI <a2369028@gmail.com>
2023-08-15 21:35:51 +0800

    feat: enum impl

--- src/enums.rs
+++ src/enums.rs
@@ -1,18 +1,23 @@
+#[derive(Debug)]
 enum Message {
     Quit,
     Color(u8, u8, u8),
     Move { x: i32, y: i32 },
     Write(String),
+    Num(i32),
 }
 
-fn match_msg(m: &Message) {
-    match m {
-        Message::Quit => println!("Quit"),
-        Message::Color(r, g, b) => println!("Color: {}, {}, {}", r, g, b),
-        Message::Move { x: a, y: b } => println!("X is {}, y is {}", a, b),
-        Message::Write(s) => {
-            println!("Write: {}", s);
-            println!("This is Write")
+impl Message {
+    fn call(&self) {
+        match self {
+            Message::Quit => println!("Quit"),
+            Message::Color(r, g, b) => println!("Color: {}, {}, {}", r, g, b),
+            Message::Move { x: a, y: b } => println!("X is {}, y is {}", a, b),
+            Message::Write(s) => {
+                println!("Write: {}", s);
+                println!("This is Write")
+            }
+            Message::Num(n) => println!("Num: {}", n),
         }
     }
 }
@@ -23,18 +28,32 @@ pub fn get_enum() {
     let move_msg = Message::Move { x: 1, y: 32 };
     let write = Message::Write(String::from("Hello"));
 
-    match_msg(&quit);
-    match_msg(&color);
-    match_msg(&move_msg);
-    match_msg(&write);
+    quit.call();
+    color.call();
+    move_msg.call();
+    write.call();
 
-    match_msg(&write);
+    let msg_number: Message = Message::Num(12);
+    match msg_number {
+        Message::Num(n) => println!("Number is: {}", n),
+        _ => println!("Not Write"),
+    }
+
+    match write {
+        Message::Write(s) => println!("Write: {}", s),
+        _ => println!("Not Write"),
+    }
+
+    println!("Get {:?}", msg_number);
+    //println!("Get {:?}", write); //Error
 
     //Option
-    let option_x: Option<i32> = None;
+    let option_x: Option<i32> = Some(5);
     if let Some(x) = option_x {
         println!("x is {}", x);
     } else {
         println!("x is None");
     }
+
+    println!("Get {:?}", option_x);
 }
